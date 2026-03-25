70eccf0ef3250a4c2b1d8923893f58a6d0100d35
RKNITH <raviranjankofficial@gmail.com>
2026-03-25 11:29:24 +0530

    fix db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 5000,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
